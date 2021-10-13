f9b8a36e34c8b858cc0398e9cb9fd09f49f81eb2
JohannesFell <johannesfell@yahoo.de>
2021-10-13 21:53:36 +0200

    First runnable Application

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,8 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
@@ -34,6 +36,45 @@ public class Main extends Application {
         button4.setPrefSize( 64.0,64.0 );
         button5.setPrefSize( 64.0,64.0 );
 
+        // Buttons den CSS_Style zuweisen
+        button1.getStyleClass().add("button1");
+        button2.getStyleClass().add("button2");
+        button3.getStyleClass().add("button3");
+        button4.getStyleClass().add("button4");
+        button5.getStyleClass().add("button5");
+
+        // Elemente klickbar machen - Eventhandling
+        button1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                getHostServices().showDocument("https://www.youtube.com/");
+            }
+        });
+        button2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                getHostServices().showDocument("https://www.facebook.com/login/");
+            }
+        });
+        button3.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                getHostServices().showDocument("https://www.twitter.com/");
+            }
+        });
+        button4.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                getHostServices().showDocument("https://www.amazon.de/");
+            }
+        });
+        button5.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.exit(0);
+            }
+        });
+
         // Elemente zum root Node hinzufügen
         root.getChildren().add( button1 );
         root.getChildren().add( button2 );
@@ -41,10 +82,8 @@ public class Main extends Application {
         root.getChildren().add( button4 );
         root.getChildren().add( button5 );
 
-        Parent parentScene = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Scene scene = new Scene( root, 400, 400 );
-
-        primaryStage.setTitle("WidgetShortCut");
+        scene.getStylesheets().add( getClass().getResource("application.css").toExternalForm() );
         primaryStage.setScene( scene );
         primaryStage.show();
     }
